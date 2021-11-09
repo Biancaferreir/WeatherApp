c6b5a2d210272e120bb59f51c940a07789860ccb
Bianca <bianca@Biancas-MacBook-Pro.local>
2021-11-09 12:42:44 -0300

    first

---
+++ Shared/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  Shared
+//
+//  Created by Bianca on 09/11/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
