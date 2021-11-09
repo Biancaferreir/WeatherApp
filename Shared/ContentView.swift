831b24e6229074eea508c9d691e40e5c43c51be2
Bianca <bianca@Biancas-MacBook-Pro.local>
2021-11-09 13:00:37 -0300

    first view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,69 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("sun")
+                .resizable()
+                .frame(width: 300, height: 300, alignment: .center)
+                .scaledToFit()
+                .padding(.bottom, 5.0)
+            
+            Text("Nome da Cidade")
+                .font(Font.system(size: 24))
+                .padding(.bottom, 10.0)
+            
+            HStack {
+                Text("Dia da Semana")
+                Text("|")
+                Text("mes")
+                Text("|")
+                Text("hora")
+            }
+            .padding(.bottom, 10.0)
+            
+            Text("20º")
+                .font(Font.system(size: 80))
+            
+            HStack {
+                Group {
+                    VStack {
+                        Text("10º")
+                        Image("sun")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .scaledToFit()
+                    }
+                    VStack {
+                        Text("10º")
+                        Image("sun")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .scaledToFit()
+                    }
+                    VStack {
+                        Text("10º")
+                        Image("sun")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .scaledToFit()
+                    }
+                    VStack {
+                        Text("10º")
+                        Image("sun")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .scaledToFit()
+                    }
+                    VStack {
+                        Text("10º")
+                        Image("sun")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .scaledToFit()
+                    }
+                }.padding(20.0)
+            }
+        }.ignoresSafeArea(.all, edges: .top)
     }
 }
 
